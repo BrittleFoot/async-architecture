89054007645251a286982b4cc991f0ff243f0588
BrittleFoot <buftioo@gmail.com>
2024-03-17 19:45:05 +0400

    carefull explosions

--- frontend/src/lib/components/Pyro.svelte
+++ frontend/src/lib/components/Pyro.svelte
@@ -109,10 +109,12 @@
 	}
 
 	.pyro {
-		position: relative;
+		position: fixed;
 		width: 50%;
 		height: 50%;
-		margin: auto;
+		transform: translate(-50%, -50%);
+		top: 50%;
+		left: 50%;
 	}
 
 	.pyro > .before,
